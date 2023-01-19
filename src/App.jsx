5e4f0147251d4f7c1979319b3706868d148c946d
MakoskiVictor <makoski.ed@gmail.com>
2023-01-19 00:20:41 -0300

    tateti functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,137 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import confetti from 'canvas-confetti'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+// eslint-disable-next-line no-unused-vars
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+function App () {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
+
+  function resetGame () {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const checkWinner = (boardToCheck) => {
+    // Revisamos las combinaciones para ver si hay un ganador
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    // Si no hay winner
+    return null
+  }
+
+  function checkEndGame (boardToCheck) {
+    const tie = !boardToCheck.includes(null)
+    return tie
+  }
+
+  const updateBoard = (index) => {
+    // No actualizamos si ya hay algo
+    if (board[index] || winner) return
+    // Pintamos el simbolo indicado
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    // Cambiamos el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    // Revisamos si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <main className='board'>
+      <h1>Ta Te Ti</h1>
+      <button onClick={resetGame}>Empezar de nuevo</button>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+
+      {
+        winner !== null && (
+          <section className='winner'>
+            <div className='text'>
+              <h2>
+                {
+                  winner === false
+                    ? 'Empate'
+                    : 'Ganó: '
+                }
+              </h2>
+              <header className='win'>
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
+    </main>
   )
 }
 
